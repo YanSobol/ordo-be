c483e99bbf0580d837f1c5b197235f600a2c41f3
Max Kleb <max.kleb@similarweb.com>
2021-05-04 17:24:43 +0300

    Dockerizing wip...

--- src/main/java/il/co/orgo/orgo/OrgoApplication.java
+++ src/main/java/il/co/orgo/orgo/OrgoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrgoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("GOT HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		SpringApplication.run(OrgoApplication.class, args);
 	}
 
